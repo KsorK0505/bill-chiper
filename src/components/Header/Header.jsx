fcb286d9a2485d5f9fd1b198b06b134885650321
KsorK0505 <dingdongfriend0505@gmail.com>
2025-01-14 14:56:18 -0500

    style: Make everything customized

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,113 +4,119 @@ import { ConnectButton } from "@rainbow-me/rainbowkit";
 import "@assets/css/components/Header/Header.css";
 
 function Header() {
-    const [isConnected, setConnected] = useState(false);
+  const [isConnected, setConnected] = useState(false);
 
-    return (
-        <div className="header">
-            <button className={`connect-wallet-button ${isConnected ? "connect" : ""}`}>
-                <ConnectButton.Custom>
-                {({
-                    account,
-                    chain,
-                    openAccountModal,
-                    openChainModal,
-                    openConnectModal,
-                    authenticationStatus,
-                    mounted,
-                }) => {
-                    // Note: If your app doesn't use authentication, you
-                    // can remove all 'authenticationStatus' checks
-                    const ready = mounted && authenticationStatus !== "loading";
-                    const connected =
-                    ready &&
-                    account &&
-                    chain &&
-                    (!authenticationStatus ||
-                        authenticationStatus === "authenticated");
-                    
-                    useEffect(() => {
-                        setConnected(connected);
-                    }, [connected]);
+  return (
+    <div className="header">
+      <button
+        className={`connect-wallet-button ${isConnected ? "connect" : ""}`}
+      >
+        <ConnectButton.Custom>
+          {({
+            account,
+            chain,
+            openAccountModal,
+            openChainModal,
+            openConnectModal,
+            authenticationStatus,
+            mounted,
+          }) => {
+            // Note: If your app doesn't use authentication, you
+            // can remove all 'authenticationStatus' checks
+            const ready = mounted && authenticationStatus !== "loading";
+            const connected =
+              ready &&
+              account &&
+              chain &&
+              (!authenticationStatus ||
+                authenticationStatus === "authenticated");
 
+            useEffect(() => {
+              setConnected(connected);
+            }, [connected]);
+
+            return (
+              <div
+                {...(!ready && {
+                  "aria-hidden": true,
+                  style: {
+                    opacity: 0,
+                    pointerEvents: "none",
+                    userSelect: "none",
+                    height: "100%",
+                  },
+                })}
+              >
+                {(() => {
+                  if (!connected) {
                     return (
-                    <div
-                        {...(!ready && {
-                        "aria-hidden": true,
-                        style: {
-                            opacity: 0,
-                            pointerEvents: "none",
-                            userSelect: "none",
-                            height: "100%",
-                        },
-                        })}
-                    >
-                        {(() => {
-                        if (!connected) {
-                            return (
-                            <button
-                                onClick={openConnectModal}
-                                type="button"
-                                className="h-full w-full"
-                            >
-                                Connect Wallet
-                            </button>
-                            );
-                        }
-
-                        if (chain.unsupported) {
-                            return (
-                            <button onClick={openChainModal} type="button">
-                                Wrong network
-                            </button>
-                            );
-                        }
-
-                        return (
-                            <div style={{ display: "flex", gap: 20 }}>
-                            <button
-                                onClick={openChainModal}
-                                style={{ display: "flex", alignItems: "center" }}
-                                type="button"
-                            >
-                                {chain.hasIcon && (
-                                <div
-                                    style={{
-                                    background: chain.iconBackground,
-                                    width: 40,
-                                    height: 40,
-                                    borderRadius: 999,
-                                    overflow: "hidden",
-                                    }}
-                                >
-                                    {chain.iconUrl && (
-                                    <img
-                                        alt={chain.name ?? "Chain icon"}
-                                        src={chain.iconUrl}
-                                        style={{ width: 40, height: 40 }}
-                                    />
-                                    )}
-                                </div>
-                                )}
-                            </button>
-
-                            <button onClick={openAccountModal} type="button" className="bg-gradient-to-r from-[rgba(131, 70, 227, 1)] to-[rgba(11, 55, 214, 1)] rounded-full w-full !important">
-                                {account.displayName}
-                                <br></br>
-                                {account.displayBalance
-                                ? ` (${account.displayBalance})`
-                                : ""}
-                            </button>
-                            </div>
-                        );
-                        })()}
-                    </div>
+                      <button
+                        onClick={openConnectModal}
+                        type="button"
+                        className="h-full w-full"
+                      >
+                        Connect Wallet
+                      </button>
                     );
-                }}
-                </ConnectButton.Custom>
-            </button>
-        </div>
-    );
+                  }
+
+                  if (chain.unsupported) {
+                    return (
+                      <button onClick={openChainModal} type="button">
+                        Wrong network
+                      </button>
+                    );
+                  }
+
+                  return (
+                    <div style={{ display: "flex", gap: 20 }}>
+                      <button
+                        onClick={openChainModal}
+                        style={{ display: "flex", alignItems: "center" }}
+                        type="button"
+                      >
+                        {chain.hasIcon && (
+                          <div
+                            style={{
+                              background: chain.iconBackground,
+                              width: 40,
+                              height: 40,
+                              borderRadius: 999,
+                              overflow: "hidden",
+                            }}
+                          >
+                            {chain.iconUrl && (
+                              <img
+                                alt={chain.name ?? "Chain icon"}
+                                src={chain.iconUrl}
+                                style={{ width: 40, height: 40 }}
+                              />
+                            )}
+                          </div>
+                        )}
+                      </button>
+
+                      <button
+                        onClick={openAccountModal}
+                        type="button"
+                        className="from-[rgba(131, 70, 227, 1)] to-[rgba(11, 55, 214, 1)] !important w-full rounded-full bg-gradient-to-r"
+                      >
+                        {account.displayName}
+                        <br></br>
+                        {account.displayBalance
+                          ? ` (${account.displayBalance})`
+                          : ""}
+                      </button>
+                    </div>
+                  );
+                })()}
+              </div>
+            );
+          }}
+        </ConnectButton.Custom>
+      </button>
+    </div>
+  );
 }
 
 export default Header;
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,13 +8,21 @@ export default {
         Ocean: ["Oceanwide"],
       },
       colors: {
-        'white-20': 'rgba(255, 255, 255, 0.2)',
+        "white-20": "rgba(255, 255, 255, 0.2)",
+        "white-40": "rgba(255, 255, 255, 0.4)",
       },
       borderWidth: {
-        '1': '1px',
-        '2': '2px',
-      }
-
+        1: "1px",
+        2: "2px",
+      },
+      backgroundImage: {
+        "globalStats-bg": 'url("@assets/img/globalStats-bg.png")',
+        "pluseChain-bg": 'url("@assets/img/pluseChain-bg.png")',
+        "avalanche-bg": 'url("@assets/img/avalancheStats-bg.png")',
+      },
+      borderColor: {
+        "light-blue": "rgb(60,67,107)",
+      },
     },
   },
   plugins: [],
